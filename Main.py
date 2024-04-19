1d7c1103c4f9a9e14551db79b47c942fdbc8b530
Lucas <lucas.vergara@live.com>
2024-04-18 21:43:40 -0300

    :construction: Working in progress

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ from os import system, name
 
 #Função para limar a tela a cada execução
 
-def clean_screen():
+def limpa_tela():
 
     #windows
     if name == 'nt':
@@ -12,3 +12,48 @@ def clean_screen():
     #Mac ou Linux
     else:
         _= system('clear')    
+
+#Function
+def game():
+    limpa_tela()
+
+    print("\nBem-vindo(a) ao jogo da forca!")
+    print("Advinhe qual a palavra abaixo: \n ")
+
+    #Lista de palavras para o jogo
+    palavras = ['banana', 'abacate', 'uva', 'morango', 'laranja']
+
+    #Escolhe radomicamente uma palavra
+    palavra = random.choice(palavras)
+
+    #list comprehension
+    letras_descobertas = ['_' for letra in palavra]
+
+    #Número de chances
+    chances = 6
+
+    #lista para as letras erradas
+    letras_erradas = []
+
+    #Loop enquanto número de chances for maior do que zero
+    while chances > 0:
+
+        #Print
+        print(" ".join(letras_descobertas))
+        print("\nChances restantes:", chances)
+        print("Letras erradas", " ".join(letras_erradas))
+
+        #Tentativa
+        tentativa = input("\nDigite uma letra:").lower()
+
+        #Condicional
+        if tentativa in palavra:
+            index = 0
+            
+            for letra in palavra:
+                if tentativa == letra:
+                    letras_descobertas[index] = letra
+                index +=1
+        else:
+            chances -=1
+            letras_erradas.append(tentativa)        
